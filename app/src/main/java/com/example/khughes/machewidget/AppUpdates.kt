10defd6c1cde0c4e3843f213160e8d414b73c0e0
Ken Hughes <ken.l.hughes@gmail.com>
2022-08-13 09:31:49 -0700

    Refactor Constants and AppUpdates in Kotlin.

--- app/src/main/java/com/example/khughes/machewidget/AppUpdates.kt
+++ app/src/main/java/com/example/khughes/machewidget/AppUpdates.kt
@@ -1,96 +1,107 @@
-package com.example.khughes.machewidget;
+package com.example.khughes.machewidget
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.content.Context
+import androidx.preference.PreferenceManager
+import com.example.khughes.machewidget.db.VehicleInfoDatabase
+import com.example.khughes.machewidget.db.UserInfoDatabase
+import java.io.File
 
-import androidx.preference.PreferenceManager;
-
-import com.example.khughes.machewidget.db.UserInfoDatabase;
-import com.example.khughes.machewidget.db.VehicleInfoDatabase;
-
-import java.io.File;
-import java.util.List;
-
-public class AppUpdates {
-
+object AppUpdates {
     // This method is intended to bundle various changes from older versions to the most recent.
-    public static void performUpdates(Context context) {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        String lastVersion = prefs.getString(context.getResources().getString(R.string.last_version_key), "");
+    @JvmStatic
+    fun performUpdates(context: Context) {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val lastVersion =
+            prefs.getString(context.resources.getString(R.string.last_version_key), "") as String
 
         // See if we need to upgrade anything since the last version
-        if (!lastVersion.equals("") && BuildConfig.VERSION_NAME.compareTo(lastVersion) > 0) {
-            LogFile.i(context, MainActivity.CHANNEL_ID, "running updates");
+        if (lastVersion != "" && BuildConfig.VERSION_NAME > lastVersion) {
+
+            LogFile.i(context, MainActivity.CHANNEL_ID, "running updates")
 
             // Add operations here
 
             // Re-enable OTA support on all vehicles, and add userId to settings.
-            if (lastVersion.compareTo("2022.05.25") < 0) {
-                LogFile.d(context, MainActivity.CHANNEL_ID, "running 2022.05.25 updates");
-
-                PreferenceManager.setDefaultValues(context, R.xml.settings_preferences, true);
-                SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-                String VIN = sharedPrefs.getString(context.getResources().getString(R.string.VIN_key), null);
-
-                new Thread(() -> {
-                    VehicleInfoDatabase.getInstance(context).vehicleInfoDao().updateSupportOTA();
+            if (lastVersion < "2022.05.25") {
+                LogFile.d(context, MainActivity.CHANNEL_ID, "running 2022.05.25 updates")
+                PreferenceManager.setDefaultValues(context, R.xml.settings_preferences, true)
+                val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context)
+                val VIN = sharedPrefs.getString(context.resources.getString(R.string.VIN_key), null)
+                Thread {
+                    VehicleInfoDatabase.getInstance(context).vehicleInfoDao().updateSupportOTA()
                     if (VIN != null) {
-                        VehicleInfo info = VehicleInfoDatabase.getInstance(context).vehicleInfoDao().findVehicleInfoByVIN(VIN);
-                        String userId = info.getUserId();
+                        val info = VehicleInfoDatabase.getInstance(context).vehicleInfoDao()
+                            .findVehicleInfoByVIN(VIN)
+                        var userId = info.userId
                         // Some vehicle entries had missing userID value.  If so, get the user ID from the first entry
                         // of the user database and update all vehicles
                         if (userId == null) {
-                            LogFile.d(context, MainActivity.CHANNEL_ID, "2022.05.25 update: adding user ID to vehicles");
-                            List<UserInfo> userInfo = UserInfoDatabase.getInstance(context).userInfoDao().findUserInfo();
-                            if (!userInfo.isEmpty()) {
-                                userId = userInfo.get(0).getUserId();
-                                for (VehicleInfo vehInfo : VehicleInfoDatabase.getInstance(context).vehicleInfoDao().findVehicleInfo()) {
-                                    vehInfo.setUserId(userId);
-                                    VehicleInfoDatabase.getInstance(context).vehicleInfoDao().updateVehicleInfo(vehInfo);
+                            LogFile.d(
+                                context,
+                                MainActivity.CHANNEL_ID,
+                                "2022.05.25 update: adding user ID to vehicles"
+                            )
+                            val userInfo =
+                                UserInfoDatabase.getInstance(context).userInfoDao().findUserInfo()
+                            if (userInfo.isNotEmpty()) {
+                                userId = userInfo[0].userId
+                                for (vehInfo in VehicleInfoDatabase.getInstance(context)
+                                    .vehicleInfoDao().findVehicleInfo()) {
+                                    vehInfo.userId = userId
+                                    VehicleInfoDatabase.getInstance(context).vehicleInfoDao()
+                                        .updateVehicleInfo(vehInfo)
                                 }
                             }
                         }
-                        sharedPrefs.edit().putString(context.getResources().getString(R.string.userId_key), userId).commit();
+                        sharedPrefs.edit()
+                            .putString(context.resources.getString(R.string.userId_key), userId)
+                            .commit()
                     }
-                }).start();
+                }.start()
             }
 
             // Re-enable OTA support on all vehicles, and add userId to settings.
-            if (lastVersion.compareTo("2022.05.31") < 0 || lastVersion.compareTo("2022.06.04a") < 0) {
-                PreferenceManager.setDefaultValues(context, R.xml.settings_preferences, true);
+            if (lastVersion < "2022.05.31" || lastVersion < "2022.06.04a") {
+                PreferenceManager.setDefaultValues(context, R.xml.settings_preferences, true)
             }
 
             // Re-enable OTA support on all vehicles, and add userId to settings.
-            if (lastVersion.compareTo("2022.06.20") < 0) {
-                new Thread(() -> {
-                    for (UserInfo userInfo : UserInfoDatabase.getInstance(context).userInfoDao().findUserInfo()) {
-                        userInfo.setProgramState(Constants.STATE_HAVE_TOKEN_AND_VIN);
-                        UserInfoDatabase.getInstance(context).userInfoDao().updateUserInfo(userInfo);
+            if (lastVersion < "2022.06.20") {
+                Thread {
+                    for (userInfo in UserInfoDatabase.getInstance(context).userInfoDao().findUserInfo()) {
+                        userInfo.programState = Constants.STATE_HAVE_TOKEN_AND_VIN
+                        UserInfoDatabase.getInstance(context).userInfoDao().updateUserInfo(userInfo)
                     }
-                }).start();
+                }.start()
             }
 
             // Reload vehicle images, including angles
-            if (lastVersion.compareTo("2022.07.16") < 0) {
-                File imageDir = new File(context.getDataDir(), Constants.IMAGES_FOLDER);
-                if (imageDir.exists() && imageDir.isDirectory()) {
-                    for (String file : imageDir.list()) {
-                        new File(imageDir, file).delete();
+            if (lastVersion < "2022.07.16") {
+                val imageDir = File(context.dataDir, Constants.IMAGES_FOLDER)
+                if (imageDir.exists() && imageDir.isDirectory) {
+                    for (file in imageDir.list()!!) {
+                        File(imageDir, file).delete()
                     }
                 }
-                new Thread(() -> {
-                    for (VehicleInfo vehicleInfo : VehicleInfoDatabase.getInstance(context).vehicleInfoDao().findVehicleInfo()) {
-                        UserInfo user = UserInfoDatabase.getInstance(context).userInfoDao().findUserInfo(vehicleInfo.getUserId());
+                Thread {
+                    for (vehicleInfo in VehicleInfoDatabase.getInstance(context).vehicleInfoDao()
+                        .findVehicleInfo()) {
+                        val user = UserInfoDatabase.getInstance(context).userInfoDao()
+                            .findUserInfo(vehicleInfo.userId)
                         if (user != null) {
-                            NetworkCalls.getVehicleImage(context, user.getAccessToken(), vehicleInfo.getVIN(), user.getCountry());
+                            NetworkCalls.getVehicleImage(
+                                context,
+                                user.accessToken,
+                                vehicleInfo.vin,
+                                user.country
+                            )
                         }
                     }
-                }).start();
+                }.start()
             }
         }
 
         // Update internally
-        prefs.edit().putString(context.getResources().getString(R.string.last_version_key), BuildConfig.VERSION_NAME).commit();
+        prefs.edit().putString(context.resources.getString(R.string.last_version_key), BuildConfig.VERSION_NAME).commit()
     }
-
 }
--- app/src/main/java/com/example/khughes/machewidget/Constants.kt
+++ app/src/main/java/com/example/khughes/machewidget/Constants.kt
@@ -1,49 +1,45 @@
-package com.example.khughes.machewidget;
+package com.example.khughes.machewidget
 
-import java.io.File;
+import java.io.File
 
-public class Constants {
-    public static final String APID = "71A3AD0A-CF46-4CCF-B473-FC7FE5BC4592";
-    public static final String CLIENTID = "9fb503e0-715b-47e8-adfd-ad4b7770f73b";
-    public static final String OTATIMEFORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"; // 2022-01-19T05:10:35.239+0000
-    public static final String STATUSTIMEFORMAT = "MM-dd-yyyy HH:mm:ss"; // 01-19-2022 13:00:00
-    public static final String LOCALTIMEFORMATUS = "MMM d, HH:mm z"; // Jan 29, 13:00 PST
-    public static final String LOCALTIMEFORMAT = "d MMM, HH:mm z"; // 29 Jan, 13:00 PST
-    public static final String LASTMODIFIEDFORMAT = "EEE, d MMM yyyy HH:mm:ss zzz"; // Sat, 29 Jan 2022 13:00:00 PST
-    public static final Double KMTOMILES = 0.6213711922;
-    public static final Double KPATOPSI = 0.14503774;
-    public static final Double KPATOBAR = 0.01;
-
-    public static final String STATE_INITIAL_STATE = "INITIAL_STATE";
-    public static final String STATE_ATTEMPT_TO_GET_ACCESS_TOKEN = "ATTEMPT_TO_GET_ACCESS_TOKEN";
-    public static final String STATE_ATTEMPT_TO_REFRESH_ACCESS_TOKEN = "ATTEMPT_TO_REFRESH_ACCESS_TOKEN";
-    public static final String STATE_HAVE_TOKEN = "HAVE_TOKEN";
-    public static final String STATE_HAVE_TOKEN_AND_VIN = "HAVE_TOKEN_AND_VIN";
-
-    public static final int HTTP_BAD_REQUEST = 400;
-    public static final int HTTP_UNAUTHORIZED = 401;
-    public static final int HTTP_SERVER_ERROR = 402;
+object Constants {
+    const val APID = "71A3AD0A-CF46-4CCF-B473-FC7FE5BC4592"
+    const val CLIENTID = "9fb503e0-715b-47e8-adfd-ad4b7770f73b"
+    const val OTATIMEFORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ" // 2022-01-19T05:10:35.239+0000
+    const val STATUSTIMEFORMAT = "MM-dd-yyyy HH:mm:ss" // 01-19-2022 13:00:00
+    const val LOCALTIMEFORMATUS = "MMM d, HH:mm z" // Jan 29, 13:00 PST
+    const val LOCALTIMEFORMAT = "d MMM, HH:mm z" // 29 Jan, 13:00 PST
+    const val LASTMODIFIEDFORMAT = "EEE, d MMM yyyy HH:mm:ss zzz" // Sat, 29 Jan 2022 13:00:00 PST
+    const val KMTOMILES = 0.6213711922
+    const val KPATOPSI = 0.14503774
+    const val KPATOBAR = 0.01
+    const val STATE_INITIAL_STATE = "INITIAL_STATE"
+    const val STATE_ATTEMPT_TO_GET_ACCESS_TOKEN = "ATTEMPT_TO_GET_ACCESS_TOKEN"
+    const val STATE_ATTEMPT_TO_REFRESH_ACCESS_TOKEN = "ATTEMPT_TO_REFRESH_ACCESS_TOKEN"
+    const val STATE_HAVE_TOKEN = "HAVE_TOKEN"
+    const val STATE_HAVE_TOKEN_AND_VIN = "HAVE_TOKEN_AND_VIN"
+    const val HTTP_BAD_REQUEST = 400
+    const val HTTP_UNAUTHORIZED = 401
+    const val HTTP_SERVER_ERROR = 402
 
     // Make sure these match values in arrays.xml
-    public static final int UNITS_SYSTEM = 0;
-    public static final int UNITS_METRIC = 1;
-    public static final int UNITS_IMPERIAL = 2;
+    const val UNITS_SYSTEM = 0
+    const val UNITS_METRIC = 1
+    const val UNITS_IMPERIAL = 2
 
-    public static final String REPOURL = "https://github.com/khpylon/MachEWidget";
+    const val REPOURL = "https://github.com/khpylon/MachEWidget"
+    const val FSVERSION_1 = "FSVERSION_1"
+    const val SHAREDPREFS_FOLDER = "shared_prefs"
+    @JvmField
+    val IMAGES_FOLDER = SHAREDPREFS_FOLDER + File.separator + "images"
+    const val WIDGET_FILE = "widget"
+    const val VIN_KEY = "VIN_"
 
-    public static final String FSVERSION_1 = "FSVERSION_1";
+    const val TEMP_ACCOUNT = "temporary"
 
-    public static final String SHAREDPREFS_FOLDER = "shared_prefs";
-    public static final String IMAGES_FOLDER = SHAREDPREFS_FOLDER + File.separator + "images";
-    public static final String WIDGET_FILE = "widget";
-    public static final String VIN_KEY = "VIN_";
-
-
-    public static final String TEMP_ACCOUNT = "temporary";
-
-    public static final String APPLICATION_JSON = "application/json";
-    public static final String APPLICATION_ZIP = "application/zip";
-    public static final String APPLICATION_OCTETSTREAM = "application/octet-stream";
-    public static final String TEXT_HTML = "text/html";
-    public static final String TEXT_PLAINTEXT = "text/plain";
+    const val APPLICATION_JSON = "application/json"
+    const val APPLICATION_ZIP = "application/zip"
+    const val APPLICATION_OCTETSTREAM = "application/octet-stream"
+    const val TEXT_HTML = "text/html"
+    const val TEXT_PLAINTEXT = "text/plain"
 }
