fa00b8f33383b346df665a3e2f2c986899b9aad4
Ken Hughes <ken.l.hughes@gmail.com>
2022-08-26 15:20:00 -0700

    Refactor OTAInfo in Kotlin. Change OTA DAO function name.

--- app/src/main/java/com/example/khughes/machewidget/OTAInfo.kt
+++ app/src/main/java/com/example/khughes/machewidget/OTAInfo.kt
@@ -1,101 +1,43 @@
-package com.example.khughes.machewidget;
+package com.example.khughes.machewidget
 
-import androidx.room.ColumnInfo;
-import androidx.room.Embedded;
-import androidx.room.Entity;
-import androidx.room.PrimaryKey;
-
-import com.example.khughes.machewidget.OTAStatus.FuseResponse;
-import com.example.khughes.machewidget.OTAStatus.FuseResponseList;
-import com.example.khughes.machewidget.OTAStatus.LanguageText;
-import com.example.khughes.machewidget.OTAStatus.OTAStatus;
-
-import java.util.ArrayList;
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+import androidx.room.Embedded
+import androidx.room.Entity
+import com.example.khughes.machewidget.OTAStatus.FuseResponseList
+import com.example.khughes.machewidget.OTAStatus.LanguageText
+import com.example.khughes.machewidget.OTAStatus.OTAStatus
+import com.example.khughes.machewidget.OTAStatus.FuseResponse
+import java.util.ArrayList
 
 @Entity(tableName = "ota_info")
-public class OTAInfo {
-
-    public OTAInfo(String VIN) {
-        this.VIN = VIN;
-        id = 0;
-    }
-
+class OTAInfo(var VIN: String) {
     @PrimaryKey(autoGenerate = true)
-    private int id;
-
-    private String VIN;
+    var id = 0
 
     @Embedded(prefix = "ota_")
-    private FuseResponseList responseList;
+    var responseList: FuseResponseList? = null
 
     @Embedded(prefix = "ota_")
-    private LanguageText languageText;
+    var languageText: LanguageText? = null
 
-    private String ota_AlertStatus;
-
-    public int getId() {
-        return id;
+    @ColumnInfo(name = "ota_alertStatus")
+    var alertStatus: String? = null
+    fun fromOTAStatus(status: OTAStatus) {
+        responseList = status.fuseResponse.fuseResponseList[0]
+        alertStatus = status.otaAlertStatus
+        languageText = status.fuseResponse.languageText
     }
 
-    public void setId(int id) {
-        this.id = id;
+    fun toOTAStatus(): OTAStatus {
+        val status = OTAStatus()
+        val list = ArrayList<FuseResponseList?>()
+        list.add(responseList)
+        val tmp = FuseResponse()
+        tmp.fuseResponseList = list
+        status.fuseResponse = tmp
+        status.otaAlertStatus = alertStatus
+        status.fuseResponse.languageText = languageText
+        return status
     }
-
-    public String getVIN() {
-        return VIN;
-    }
-
-    public void setVIN(String VIN) {
-        this.VIN = VIN;
-    }
-
-    public FuseResponseList getResponseList() {
-        return responseList;
-    }
-
-    public void setResponseList(FuseResponseList responseList) {
-        this.responseList = responseList;
-    }
-
-    public String getOtaAlertStatus() {
-        return ota_AlertStatus;
-    }
-
-    public void setOtaAlertStatus(String otaAlertStatus) {
-        this.ota_AlertStatus = otaAlertStatus;
-    }
-
-    public String getOta_AlertStatus() { return ota_AlertStatus; }
-
-    public LanguageText getLanguageText() {
-        return languageText;
-    }
-
-    public void setLanguageText(LanguageText languageText) {
-        this.languageText = languageText;
-    }
-
-    public void setOta_AlertStatus(String ota_AlertStatus) {
-        this.ota_AlertStatus = ota_AlertStatus;
-    }
-
-    public void fromOTAStatus(OTAStatus status) {
-        this.responseList = status.getFuseResponse().getFuseResponseList().get(0);
-        this.ota_AlertStatus = status.getOtaAlertStatus();
-        this.languageText = status.getFuseResponse().getLanguageText();
-    }
-
-    public OTAStatus toOTAStatus() {
-        OTAStatus status = new OTAStatus();
-
-        ArrayList<FuseResponseList> list = new ArrayList<>();
-        list.add(this.responseList);
-        FuseResponse tmp = new FuseResponse();
-        tmp.setFuseResponseList(list);
-        status.setFuseResponse(tmp);
-        status.setOtaAlertStatus(this.ota_AlertStatus);
-        status.getFuseResponse().setLanguageText(this.languageText);
-        return status;
-    }
-
 }
--- app/src/main/java/com/example/khughes/machewidget/db/OTAInfoDao.java
+++ app/src/main/java/com/example/khughes/machewidget/db/OTAInfoDao.java
@@ -19,34 +19,10 @@ public interface OTAInfoDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertOTAInfo(OTAInfo info);
 
-    @Query("SELECT * FROM ota_info WHERE ota_oemCorrelationId LIKE :correlationId AND vin LIKE :VIN" )
-    List<OTAInfo> findOTAStatusByCorrelationId(String correlationId, String VIN);
+    @Query("SELECT * FROM ota_info WHERE ota_oemCorrelationId LIKE :correlationId AND vin LIKE :VIN "
+            + "ORDER BY ota_dateTimestamp ASC" )
+    List<OTAInfo> findOTAInfoByCorrelationId(String correlationId, String VIN);
 
     @Update
     void updateOTAInfo(OTAInfo info);
-
-//    @Query("SELECT * FROM ota_status WHERE vin LIKE :VIN")
-//    VehicleInfo findVehicleInfoByVIN(String VIN);
-//
-//    @Query("SELECT vin FROM vehicle_info WHERE userId LIKE :userId")
-//    List<String> findVINsByUserId(String userId);
-//
-//    @Query("SELECT * FROM vehicle_info WHERE userId LIKE :userId")
-//    List<VehicleInfo> findVehicleInfoByUserId(String userId);
-//
-//    @Query("SELECT nickname, VIN, enabled FROM vehicle_info")
-//    LiveData<List<VehicleIds>> getLiveDataVehicleInfo();
-//
-//    @Query("DELETE FROM vehicle_info WHERE vin LIKE :VIN")
-//    void deleteVehicleInfoByVIN(String VIN);
-//
-//    @Query("DELETE FROM vehicle_info WHERE userId LIKE :userId")
-//    void deleteVehicleInfoByUserId(String userId);
-//
-//    @Query("UPDATE vehicle_info SET supportsOTA = 1")
-//    void updateSupportOTA();
-//
-//    @Query("UPDATE vehicle_info SET enabled = :value WHERE vin = :VIN")
-//    void updateEnable(String VIN, boolean value);
-//
 }
