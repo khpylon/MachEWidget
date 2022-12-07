d5420f52417cd38fb6c60a7265956555f078e32f
Ken Hughes <ken.l.hughes@gmail.com>
2022-12-07 09:20:15 -0800

    On boot, don't update settings if manual updates are are in use.

--- app/src/main/java/com/example/khughes/machewidget/BootComplete.kt
+++ app/src/main/java/com/example/khughes/machewidget/BootComplete.kt
@@ -6,6 +6,7 @@ import android.app.PendingIntent
 import android.app.AlarmManager
 import android.content.Context
 import android.text.format.DateUtils
+import androidx.preference.PreferenceManager
 
 class BootComplete : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
@@ -15,22 +16,36 @@ class BootComplete : BroadcastReceiver() {
         ) {
             LogFile.d(context, MainActivity.CHANNEL_ID, "BootComplete received action $action")
 
-            // Check the car's status as soon as possible.
-            val pendingIntent =
-                PendingIntent.getBroadcast(
-                    context, 0,
-                    Intent(context, StatusReceiver::class.java),
-                    PendingIntent.FLAG_IMMUTABLE
+            val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+            val delayInMillis = sharedPref.getString(
+                context.resources.getString(R.string.update_frequency_key),
+                "15"
+            )!!.toInt()
+
+            if (delayInMillis > 0) {
+                // Check the car's status as soon as possible.
+                val pendingIntent =
+                    PendingIntent.getBroadcast(
+                        context, 0,
+                        Intent(context, StatusReceiver::class.java),
+                        PendingIntent.FLAG_IMMUTABLE
+                    )
+                val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                alarmManager[AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + DateUtils.SECOND_IN_MILLIS] =
+                    pendingIntent
+                alarmManager.setWindow(
+                    AlarmManager.RTC_WAKEUP,
+                    System.currentTimeMillis() + DateUtils.SECOND_IN_MILLIS * 15,
+                    DateUtils.SECOND_IN_MILLIS,
+                    pendingIntent
                 )
-            val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-            alarmManager[AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + DateUtils.SECOND_IN_MILLIS] =
-                pendingIntent
-            alarmManager.setWindow(
-                AlarmManager.RTC_WAKEUP,
-                System.currentTimeMillis() + DateUtils.SECOND_IN_MILLIS * 15,
-                DateUtils.SECOND_IN_MILLIS,
-                pendingIntent
-            )
+            } else {
+                LogFile.d(
+                    context,
+                    MainActivity.CHANNEL_ID,
+                    "Manual updates enabled, so alarm not set."
+                )
+            }
 
             // Check for updates (github version only).
             UpdateReceiver.createIntent(context)
