46ba803e859c57308132db98cc3826a9d8e5e7ea
Ken Hughes <ken.l.hughes@gmail.com>
2023-03-21 10:19:32 -0700

    Refactor Converters in Kotlin

---
+++ app/src/main/java/com/example/khughes/machewidget/db/Converters.kt
@@ -0,0 +1,23 @@
+package com.example.khughes.machewidget.db
+
+import androidx.room.TypeConverter
+import com.example.khughes.machewidget.OTAStatus.FuseResponseList
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+object Converters {
+    var gson = Gson()
+    @TypeConverter
+    fun stringToFuseResponseList(data: String?): List<FuseResponseList> {
+        if (data == null) {
+            return emptyList()
+        }
+        val listType = object : TypeToken<List<FuseResponseList?>?>() {}.type
+        return gson.fromJson(data, listType)
+    }
+
+    @TypeConverter
+    fun fuseResponseListToString(list: List<FuseResponseList?>?): String {
+        return gson.toJson(list)
+    }
+}
