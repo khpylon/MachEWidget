da608f9410a456efa6fb452360189fb6847ddc2f
Ken Hughes <ken.l.hughes@gmail.com>
2024-01-17 14:49:58 -0800

    New first-level authentication code.

--- app/src/main/java/com/example/khughes/machewidget/Authenticate.java
+++ app/src/main/java/com/example/khughes/machewidget/Authenticate.java
@@ -18,8 +18,6 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Base64;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -61,16 +59,16 @@ public class Authenticate {
 
     private static String generateCodeVerifier() throws UnsupportedEncodingException {
         SecureRandom secureRandom = new SecureRandom();
-        byte[] codeVerifier = new byte[32];
-        secureRandom.nextBytes(codeVerifier);
-        return Base64.getUrlEncoder().withoutPadding().encodeToString(codeVerifier);
+        byte[] code1 = new byte[32];
+        secureRandom.nextBytes(code1);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(code1);
     }
 
-    private static String generateCodeChallange(String codeVerifier) {
+    private static String generateCodeChallange(String code1) {
         byte[] bytes;
         byte[] digest = new byte[0];
         try {
-            bytes = codeVerifier.getBytes(StandardCharsets.US_ASCII);
+            bytes = code1.getBytes(StandardCharsets.US_ASCII);
             MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
             messageDigest.update(bytes, 0, bytes.length);
             digest = messageDigest.digest();
@@ -105,176 +103,119 @@ public class Authenticate {
                     .build();
             List<String> cookies = new ArrayList<>();
 
-            String codeVerifier = generateCodeVerifier();
-            String codeChallenge = generateCodeChallange(codeVerifier);
+            String code1 = generateCodeVerifier();
+            String codeVerifier = generateCodeChallange(code1);
 
-            // first URL
-            String url1 = "https://sso.ci.ford.com/v1.0/endpoint/default/authorize?redirect_uri=fordapp://userauthorized&response_type=code&scope=openid&max_age=3600&client_id=9fb503e0-715b-47e8-adfd-ad4b7770f73b&code_challenge="
-                    + codeChallenge + "&code_challenge_method=S256";
+            String FORD_LOGIN_URL = "https://login.ford.com/";
+
+            String country_code = "EN-US";
+            String short_country_code = "USA";
+            String url1 = FORD_LOGIN_URL + "4566605f-43a7-400a-946e-89cc9fdb0bd7/B2C_1A_SignInSignUp_"+country_code+
+                    "/oauth2/v2.0/authorize?redirect_uri=fordapp://userauthorized&response_type=code&max_age=3600&"+
+                    "scope=%2009852200-05fd-41f6-8c21-d36d3497dc64%20openid&client_id=09852200-05fd-41f6-8c21-d36d3497dc64&"+
+                    "code_challenge="+codeVerifier+
+                    "&code_challenge_method=S256&ui_locales="+country_code+
+                    "&language_code="+country_code+
+                    "&country_code="+short_country_code+
+                    "&ford_application_id=5C80A6BB-CF0D-4A30-BDBF-FC804B5C1A98";
+
+            Headers loginHeaders = new Headers.Builder()
+                    .add("Accept-Language", "en-US")
+                    .add("Accept", "*/*")
+                    .add("User-Agent","Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Mobile/15E148 Safari/604.1")
+                    .build();
 
             Request request = new Request.Builder()
                     .url(url1)
-                    .headers(defaultHeaders)
-                    .addHeader("Content-type", "application/json")
+                    .headers(loginHeaders)
                     .build();
             okhttp3.Response response = client.newCall(request).execute();
+
+            if (response.code() != 200) {
+                LogFile.e(context, MainActivity.CHANNEL_ID, "Authorization failed: first GET request didn't return 200 response");
+                return null;
+            }
+
+            String responseBody = response.body().string().replaceAll("\"", "'");
+
+            RequestBody requestBody;
+            Headers.Builder headers;
+
+            int start = responseBody.indexOf("{", responseBody.indexOf("var SETTINGS = ") );
+            int ending = responseBody.lastIndexOf("}", responseBody.indexOf("\r", start));
+
+            String json = responseBody.substring(start, ending+1);
+            Gson ggson = new GsonBuilder().create();
+            Map settings = ggson.fromJson(json, Map.class);
+
+            String transId = settings.get("transId").toString();
+            String csrfToken = settings.get("csrf").toString();
+
+            String urlp = FORD_LOGIN_URL + "4566605f-43a7-400a-946e-89cc9fdb0bd7/B2C_1A_SignInSignUp_" + country_code +
+                    "/SelfAsserted?tx="+ transId +
+                    "&p=B2C_1A_SignInSignUp_en-AU";
+
+            requestBody = new FormBody.Builder()
+                    .add("request_type", "RESPONSE")
+                    .add("signInName", username)
+                    .add("password", password)
+                    .build();
+
+            request = new Request.Builder()
+                    .url(urlp)
+                    .post(requestBody)
+                    .headers(loginHeaders)
+                    .addHeader("Origin", "https://login.ford.com")
+                    .addHeader("Referer", url1)
+                    .addHeader("X-Csrf-Token", csrfToken)
+                    .build();
+            client = ssoCiHttpClient
+                    .cookieJar(cookieJar)
+                    .build();
+            response = client.newCall(request).execute();
+
+            if (response.code() != 200) {
+                LogFile.e(context, MainActivity.CHANNEL_ID, "Authorization failed: first GET request didn't return 200 response");
+                return null;
+            }
+
+            cookies.addAll(response.headers().values("set-cookie"));
+            String newUrl = FORD_LOGIN_URL +
+                    "4566605f-43a7-400a-946e-89cc9fdb0bd7/B2C_1A_SignInSignUp_" + country_code +
+                    "/api/CombinedSigninAndSignup/confirmed?rememberMe=false&csrf_token=" + csrfToken;
+
+            request = new Request.Builder()
+                    .url(newUrl)
+                    .headers(loginHeaders)
+                    .build();
+            client = ssoCiHttpClient
+                    .cookieJar(cookieJar)
+                    .followRedirects(false)
+                    .build();
+            response = client.newCall(request).execute();
+
             if (response.code() != 302) {
                 LogFile.e(context, MainActivity.CHANNEL_ID, "Authorization failed: first GET request didn't return 302 response");
                 return null;
             }
 
-            RequestBody requestBody;
-            Headers.Builder headers;
-
-            cookies.addAll(response.headers().values("set-cookie"));
-            String newUrl = response.headers().get("location");
-
-            if (!newUrl.startsWith("fordapp://userauthorized")) {
-                LogFile.d(context, MainActivity.CHANNEL_ID, "302 redirect found: proceeding with long login");
-
-                // Second URL
-                headers = new Headers.Builder();
-                headers.addAll(defaultHeaders);
-                for (String cookie : cookies) {
-                    headers.add("cookie", cookie);
-                }
-                request = new Request.Builder()
-                        .url(newUrl)
-                        .headers(headers.build())
-                        .build();
-                client = ssoCiHttpClient
-                        .cookieJar(cookieJar)
-                        .build();
-                response = client.newCall(request).execute();
-                if (response.code() != 302) {
-                    LogFile.e(context, MainActivity.CHANNEL_ID, "Authorization failed: second GET request didn't return 302 response");
-                    return null;
-                }
-                cookies.addAll(response.headers().values("set-cookie"));
-                newUrl = response.headers().get("location");
-
-                // Third URL - this should return a 200 code and be the actual login webpage
-                headers = new Headers.Builder();
-                headers.addAll(defaultHeaders);
-                for (String cookie : cookies) {
-                    headers.add("cookie", cookie);
-                }
-                request = new Request.Builder()
-                        .url(newUrl)
-                        .headers(headers.build())
-                        .build();
-                response = client.newCall(request).execute();
-                if (response.code() != 200) {
-                    LogFile.e(context, MainActivity.CHANNEL_ID, "Authorization failed: third GET request didn't return 200 response");
-                    return null;
-                }
-
-                String thing = response.body().string();
-                final String loginUrlString = "data-ibm-login-url=\"";
-                thing = thing.substring(thing.indexOf(loginUrlString) + loginUrlString.length() + 1);
-                thing = thing.substring(0, thing.indexOf("\""));
-
-                // Fourth URL - perform the login with our credentials
-                newUrl = "https://sso.ci.ford.com/" + thing;
-
-                requestBody = new FormBody.Builder()
-                        .add("operation", "verify")
-                        .add("login-form-type", "password")
-                        .add("username", username)
-                        .add("password", password)
-                        .build();
-
-                request = new Request.Builder()
-                        .url(newUrl)
-                        .post(requestBody)
-                        .headers(defaultHeaders)
-                        .addHeader("application-id", applicationId)
-                        .addHeader("Content-Type", "application/x-www-form-urlencoded")
-                        .build();
-                client = ssoCiHttpClient
-                        .cookieJar(cookieJar)
-                        .build();
-                response = client.newCall(request).execute();
-                if (response.code() == 200) {
-                    thing = response.body().string();
-                    final String errorString = "data-ibm-login-error-text=\"";
-                    thing = thing.substring(thing.indexOf(errorString) + errorString.length());
-//                    thing = thing.substring(0, thing.indexOf("\""));
-                    if (thing.startsWith(ACCOUNT_BAD_USER_OR_PASSWORD)) {
-                        return ACCOUNT_BAD_USER_OR_PASSWORD;
-                    }
-                    else if (thing.startsWith(ACCOUNT_DISABLED_CODE)) {
-                        return ACCOUNT_DISABLED_CODE;
-                    }
-                }
-                if (response.code() != 302) {
-                    LogFile.e(context, MainActivity.CHANNEL_ID, "Authorization failed: fourth POST request didn't return 302 response");
-                    return null;
-                }
-                cookies.addAll(response.headers().values("set-cookie"));
-                newUrl = response.headers().get("location");
-
-                // Fifth URL
-                headers = new Headers.Builder();
-                headers.addAll(defaultHeaders);
-                for (String cookie : cookies) {
-                    headers.add("cookie", cookie);
-                }
-                request = new Request.Builder()
-                        .url(newUrl)
-                        .headers(headers.build())
-                        .build();
-                response = client.newCall(request).execute();
-                if (response.code() != 302) {
-                    LogFile.e(context, MainActivity.CHANNEL_ID, "Authorization failed: fifth GET request didn't return 302 response");
-                    return null;
-                }
-
-                newUrl = response.headers().get("location");
-                if (!newUrl.startsWith("fordapp://userauthorized")) {
-                    LogFile.e(context, MainActivity.CHANNEL_ID, "Authorization failed: fifth GET request doesn't contain location \"fordapp://userauthorized\"");
-                    return null;
-                }
-                cookies.addAll(response.headers().values("set-cookie"));
-            }
-
-            final Map<String, List<String>> query_pairs = new LinkedHashMap<String, List<String>>();
-            final String[] pairs = newUrl.split("\\?")[1].split("&");
-            for (String pair : pairs) {
-                final int idx = pair.indexOf("=");
-                final String key = idx > 0 ? URLDecoder.decode(pair.substring(0, idx), "UTF-8") : pair;
-                if (!query_pairs.containsKey(key)) {
-                    query_pairs.put(key, new LinkedList<String>());
-                }
-                final String value = idx > 0 && pair.length() > idx + 1 ? URLDecoder.decode(pair.substring(idx + 1), "UTF-8") : null;
-                query_pairs.get(key).add(value);
-            }
-            String myCode = query_pairs.get("code").get(0);
-            String myGrantID = query_pairs.get("grant_id").get(0);
-
-            newUrl = "https://sso.ci.ford.com/oidc/endpoint/default/token";
+            newUrl = response.headers().get("location").replace("fordapp://userauthorized/?code=","");
 
             requestBody = new FormBody.Builder()
-                    .add("client_id", clientId)
+                    .add("client_id", "09852200-05fd-41f6-8c21-d36d3497dc64")
                     .add("grant_type", "authorization_code")
+                    .add("code_verifier", code1)
+                            .add("code", newUrl)
                     .add("redirect_uri", "fordapp://userauthorized")
-                    .add("grant_id", myGrantID)
-                    .add("code", myCode)
-                    .add("code_verifier", codeVerifier)
                     .build();
 
-            headers = new Headers.Builder();
-            headers.addAll(defaultHeaders);
-            for (String cookie : cookies) {
-                headers.add("cookie", cookie);
-            }
-
             request = new Request.Builder()
-                    .url(newUrl)
-                    .headers(headers.build())
-                    .addHeader("application-id", applicationId)
-                    .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                    .url(FORD_LOGIN_URL + "4566605f-43a7-400a-946e-89cc9fdb0bd7/B2C_1A_SignInSignUp_" + country_code + "/oauth2/v2.0/token")
                     .post(requestBody)
+                    .headers(loginHeaders)
+                    .addHeader("Origin", "https://login.ford.com")
+                    .addHeader("Referer", url1)
+                    .addHeader("X-Csrf-Token", csrfToken)
                     .build();
             client = ssoCiHttpClient
                     .cookieJar(cookieJar)
@@ -284,12 +225,11 @@ public class Authenticate {
             if (response.code() != 200) {
                 LogFile.e(context, MainActivity.CHANNEL_ID, "Authorization failed: final POST request didn't return 200 response");
                 return null;
-            } else {
-                LogFile.d(context, MainActivity.CHANNEL_ID, "Authorization successful");
-                Gson gson = new GsonBuilder().create();
-                AccessToken accessToken = gson.fromJson(response.body().string(), AccessToken.class);
-                return accessToken.getAccessToken();
             }
+            LogFile.d(context, MainActivity.CHANNEL_ID, "Authorization successful");
+            Gson gson = new GsonBuilder().create();
+            AccessToken accessToken = gson.fromJson(response.body().string(), AccessToken.class);
+            return accessToken.getAccessToken();
         } catch (IOException e) {
             LogFile.e(context, MainActivity.CHANNEL_ID, "exception in Authenticate.newAuthenticate: ", e);
         }
