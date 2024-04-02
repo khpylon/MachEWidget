9fd938665445c97c205946158111040b09289b6c
Ken Hughes <ken.l.hughes@gmail.com>
2024-04-02 15:09:25 -0700

    Refactor Login Activity in Kotlin, and add language translations.

--- app/src/main/java/com/example/khughes/machewidget/LoginActivity.kt
+++ app/src/main/java/com/example/khughes/machewidget/LoginActivity.kt
@@ -1,102 +1,115 @@
-package com.example.khughes.machewidget;
+package com.example.khughes.machewidget
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.widget.Button;
-import android.widget.Toast;
+import android.content.Intent
+import android.icu.text.MessageFormat
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
+import com.example.khughes.machewidget.LogFile.i
+import com.example.khughes.machewidget.NetworkCalls.Companion.getAccessToken
+import com.example.khughes.machewidget.StatusReceiver.Companion.nextAlarm
+import com.google.android.material.textfield.TextInputLayout
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceManager;
+class LoginActivity : AppCompatActivity() {
+    private var usernameWidget: TextInputLayout? = null
+    private var passwordWidget: TextInputLayout? = null
 
-import com.google.android.material.textfield.TextInputLayout;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-public class LoginActivity extends AppCompatActivity {
+        setContentView(R.layout.activity_login)
 
-    private TextInputLayout usernameWidget, passwordWidget;
+        usernameWidget = findViewById(R.id.username)
+        passwordWidget = findViewById(R.id.password)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_login);
-
-        usernameWidget = findViewById(R.id.username);
-        passwordWidget = findViewById(R.id.password);
-
-        Button login = findViewById(R.id.login);
-        login.setOnClickListener(view -> {
-            if (usernameWidget == null || usernameWidget.getEditText() == null ||
-                    passwordWidget == null || passwordWidget.getEditText() == null) {
-                return;
+        val login = findViewById<Button>(R.id.login)
+        login.setOnClickListener { view: View? ->
+            if (usernameWidget == null || usernameWidget!!.editText == null || passwordWidget == null || passwordWidget!!.editText == null) {
+                return@setOnClickListener
             }
-
-            String username = usernameWidget.getEditText().getText().toString();
-            String password = passwordWidget.getEditText().getText().toString();
-
-            if (username.length() == 0) {
-                Toast.makeText(getApplicationContext(), "Please enter a username.", Toast.LENGTH_SHORT).show();
-            } else if (password.length() == 0) {
-                Toast.makeText(getApplicationContext(), "Please enter a password.", Toast.LENGTH_SHORT).show();
+            val username = usernameWidget!!.editText!!.text.toString()
+            val password = passwordWidget!!.editText!!.text.toString()
+            if (username.length == 0) {
+                Toast.makeText(applicationContext,
+                    getString(R.string.activity_login_enter_username_toast), Toast.LENGTH_SHORT)
+                    .show()
+            } else if (password.length == 0) {
+                Toast.makeText(applicationContext,
+                    getString(R.string.activity_login_enter_password_toast), Toast.LENGTH_SHORT)
+                    .show()
             } else {
-                getAccess(username, password);
+                getAccess(username, password)
             }
-        });
+        }
     }
 
-    private void getAccess(String username, String password) {
-        Context context = getApplicationContext();
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+    private fun getAccess(username: String, password: String) {
+        val context = applicationContext
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
 
-        Handler h = new Handler(Looper.getMainLooper()) {
-            @Override
-            public void handleMessage(Message msg) {
-                Bundle bb = msg.getData();
-                String action = bb.getString("action");
-                LogFile.i(context, MainActivity.CHANNEL_ID, "Access: " + action);
+        val h: Handler = object : Handler(Looper.getMainLooper()) {
+            override fun handleMessage(msg: Message) {
+                val bb = msg.data
+                val action = bb.getString("action")
+                i(context, MainActivity.CHANNEL_ID, "Access: $action")
 
-                switch (action) {
+                when (action) {
+                    Constants.STATE_HAVE_TOKEN_AND_VIN -> {
+                        Toast.makeText(
+                            applicationContext,
+                            getString(R.string.activity_login_login_successful_toast),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        nextAlarm(context, 5)
+                        val data = Intent()
+                        setResult(RESULT_OK, data)
+                        finish()
+                    }
 
-                    // Log in was successful, and vehicles associated with user ID exists, so update them.
-                    case Constants.STATE_HAVE_TOKEN_AND_VIN:
-                        Toast.makeText(getApplicationContext(), "Login successful; updating in 5 seconds.", Toast.LENGTH_SHORT).show();
-                        StatusReceiver.nextAlarm(context, 5);
-                        Intent data = new Intent();
-                        setResult(Activity.RESULT_OK, data);
-                        finish();
-                        break;
-
-                    // Log in was successful but no vehicles found
-                    case Constants.STATE_HAVE_TOKEN:
+                    Constants.STATE_HAVE_TOKEN -> {
                         // Update global userId key
-                        String userId = bb.getString("userId");
-                        sharedPref.edit().putString(context.getResources().getString(R.string.userId_key), userId).apply();
+                        val userId = bb.getString("userId")
+                        sharedPref.edit()
+                            .putString(context.resources.getString(R.string.userId_key), userId)
+                            .apply()
 
                         // Prompt the user to add vehicles
-                        Intent intent = new Intent(context, VehicleActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent);
-                        finish();
-                        break;
+                        val intent = Intent(context, VehicleActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                        startActivity(intent)
+                        finish()
+                    }
 
-                    case Constants.STATE_ACCOUNT_DISABLED:
-                        Toast.makeText(getApplicationContext(), "Your account has been disabled (error "
-                                + Authenticate.ACCOUNT_DISABLED_CODE + "): contact Ford via the FordPass app to reactivate.", Toast.LENGTH_LONG).show();
-                        break;
+                    Constants.STATE_ACCOUNT_DISABLED -> Toast.makeText(
+                        applicationContext,
+                        MessageFormat.format (
+                            getString(R.string.activity_login_account_disabled_error_toast),
+                                 Authenticate.ACCOUNT_DISABLED_CODE ),
+                        Toast.LENGTH_LONG
+                    ).show()
 
-                    case Constants.STATE_ATTEMPT_TO_GET_ACCESS_TOKEN:
-                    default:
-                        Toast.makeText(getApplicationContext(), "Unable to login to server: check your username and/or password?", Toast.LENGTH_LONG).show();
-                        break;
+                    Constants.STATE_ATTEMPT_TO_GET_ACCESS_TOKEN -> Toast.makeText(
+                        applicationContext,
+                        getString(R.string.activity_login_check_username__password_toast),
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    else -> Toast.makeText(
+                        applicationContext,
+                        getString(R.string.activity_login_check_username__password_toast),
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             }
-        };
-        Toast.makeText(getApplicationContext(), "Attempting to log in...", Toast.LENGTH_SHORT).show();
-        NetworkCalls.getAccessToken(h, getApplicationContext(), username, password);
+        }
+        Toast.makeText(applicationContext,
+            getString(R.string.activity_login_attempting_to_log_in_toast), Toast.LENGTH_SHORT).show()
+        getAccessToken(h, applicationContext, username, password)
     }
 }
