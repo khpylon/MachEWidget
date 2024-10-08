d8dccccc9002405fedf6ffbcc9f13f8fbb232a0c
Ken Hughes <ken.l.hughes@gmail.com>
2024-10-08 08:16:44 -0700

    Changes for JSON fields in the FordConnect API.

--- app/src/main/java/com/example/khughes/machewidget/AccessToken.kt
+++ app/src/main/java/com/example/khughes/machewidget/AccessToken.kt
@@ -14,50 +14,50 @@ class AccessToken {
     @Expose
     var refreshToken: String? = null
 
-    @SerializedName("grant_id")
-    @Expose
-    var grantId: String? = null
-
-    @SerializedName("token_type")
-    @Expose
-    var tokenType: String? = null
-
     @SerializedName("expires_in")
     @Expose
     var expiresIn: Int? = null
 
-    @SerializedName("ford_consumer_id")
+    @SerializedName("resource")
     @Expose
-    var userId: String? = null
+    var resource: String? = null
 
-    @SerializedName("profile")
-    @Expose
-    var userProfile: UserProfile? = null
-
-    @Generated("jsonschema2pojo")
-    inner class UserProfile {
-        @SerializedName("userGuid")
-        @Expose
-        var userGuid: String? = null
-
-        @SerializedName("country")
-        @Expose
-        var country: String? = null
-
-        @SerializedName("language")
-        @Expose
-        var language: String? = null
-
-        @SerializedName("uomSpeed")
-        @Expose
-        var uomSpeed: String? = null
-
-        @SerializedName("uomDistance")
-        @Expose
-        var uomDistance: Int? = null
-
-        @SerializedName("uomPressure")
-        @Expose
-        var uomPressure: String? = null
-    }
+//    @SerializedName("token_type")
+//    @Expose
+//    var tokenType: String? = null
+//
+//    @SerializedName("ford_consumer_id")
+//    @Expose
+//    var userId: String? = null
+//
+//    @SerializedName("profile")
+//    @Expose
+//    var userProfile: UserProfile? = null
+//
+//    @Generated("jsonschema2pojo")
+//    inner class UserProfile {
+//        @SerializedName("userGuid")
+//        @Expose
+//        var userGuid: String? = null
+//
+//        @SerializedName("country")
+//        @Expose
+//        var country: String? = null
+//
+//        @SerializedName("language")
+//        @Expose
+//        var language: String? = null
+//
+//        @SerializedName("uomSpeed")
+//        @Expose
+//        var uomSpeed: String? = null
+//
+//        @SerializedName("uomDistance")
+//        @Expose
+//        var uomDistance: Int? = null
+//
+//        @SerializedName("uomPressure")
+//        @Expose
+//        var uomPressure: String? = null
+//    }
 }
