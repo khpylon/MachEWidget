362f7f97f550af085813c4e0dddc45f58a516cab
Ken Hughes <ken.l.hughes@gmail.com>
2024-10-20 09:41:05 -0700

    Refactor profiles to allow user to choose a vehicle for a widget.

--- app/src/main/java/com/example/khughes/machewidget/ProfileManager.kt
+++ app/src/main/java/com/example/khughes/machewidget/ProfileManager.kt
@@ -12,48 +12,50 @@ import java.util.ArrayList
 object ProfileManager : AppCompatActivity() {
     @JvmStatic
     fun changeProfile(context: Context, widget_VIN: String?): String? {
-        // TODO: Change this to work without userId
-        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
-        val VIN = context.getSharedPreferences(Constants.WIDGET_FILE, MODE_PRIVATE)
+        val vehicleId = context.getSharedPreferences(Constants.WIDGET_FILE, MODE_PRIVATE)
             .getString(widget_VIN, null)
-//        val info = arrayOf<InfoRepository?>(null)
-//        val handler: Handler = object : Handler(Looper.getMainLooper()) {
-//            override fun handleMessage(msg: Message) {
-//                val userId =
-//                    sharedPref.getString(context.resources.getString(R.string.userId_key), "")
-//                val vehicles: MutableList<VehicleInfo> = ArrayList()
-//
-//                // Get all vehicles owned by the user
-//                var index = 0
-//                for (tmp in info[0]!!.vehicles) {
-//                    if (tmp.userId == userId) {
-//                        if (tmp.vin == VIN) {
-//                            index = vehicles.size
-//                        }
-//                        vehicles.add(tmp)
-//                    }
-//                }
-//
-//                // If there's more than one VIN, look through the list for the next enabled one
-//                if (vehicles.size > 1) {
-//                    do {
-//                        index = (index + 1) % vehicles.size
-//                    } while (!vehicles[index].isEnabled)
-//                    val newVIN = vehicles[index].vin
-//                    // If the VIN is new, apply changes.
-//                    if (VIN != newVIN) {
-//                        context.getSharedPreferences(Constants.WIDGET_FILE, MODE_PRIVATE).edit()
-//                            .putString(widget_VIN, newVIN).commit()
-//                        Toast.makeText(context, vehicles[index].nickname, Toast.LENGTH_SHORT).show()
-//                        CarStatusWidget.updateWidget(context)
-//                    }
-//                }
-//            }
-//        }
-//        Thread {
-//            info[0] = InfoRepository(context)
-//            handler.sendEmptyMessage(0)
-//        }.start()
-        return VIN
+        val info = arrayOf<InfoRepository?>(null)
+        val handler: Handler = object : Handler(Looper.getMainLooper()) {
+            override fun handleMessage(msg: Message) {
+                val vehicles: MutableList<VehicleInfo> = ArrayList()
+
+                // Get all vehicles owned by the user
+                var index = 0
+                for (tmp in info[0]!!.vehicles) {
+                    if (tmp.carStatus.vehicle.vehicleId == vehicleId) {
+                        index = vehicles.size
+                    }
+                    vehicles.add(tmp)
+                }
+
+                // If there's more than one vehicle, look through the list for the next enabled one
+                if (vehicles.size > 1) {
+                    do {
+                        index = (index + 1) % vehicles.size
+                    } while (!vehicles[index].isEnabled)
+                    val newVehicleId = vehicles[index].carStatus.vehicle.vehicleId
+                    // If the ID is new, apply changes.
+                    if (vehicleId != newVehicleId) {
+                        context.getSharedPreferences(Constants.WIDGET_FILE, MODE_PRIVATE).edit()
+                            .putString(widget_VIN, newVehicleId).commit()
+                        val nickname = vehicles[index].carStatus.vehicle.nickName
+                        if (nickname != "") {
+                            Toast.makeText(
+                                context,
+                                vehicles[index].carStatus.vehicle.nickName,
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                        }
+                        CarStatusWidget.updateWidget(context)
+                    }
+                }
+            }
+        }
+        Thread {
+            info[0] = InfoRepository(context)
+            handler.sendEmptyMessage(0)
+        }.start()
+        return vehicleId
     }
 }
